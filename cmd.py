fc186af3ea0376ed8fcb6f652a12526afc9df64c
Vitaliy Bogdanets <vitlik@gmail.com>
2025-01-09 00:00:31 +0200

    Add debug logging to CMD command

--- cmd.py
+++ cmd.py
@@ -5,22 +5,38 @@ import subprocess
 
 class OpenCmdCommand(sublime_plugin.WindowCommand):
     def run(self, dirs=None):
+        print("OpenCmdCommand.run called with dirs:", dirs)
+        
         if not dirs:
+            print("No directories provided")
             return
             
         path = dirs[0]
+        print("Selected path:", path)
         
         if not os.path.isdir(path):
             path = os.path.dirname(path)
+            print("Using parent directory:", path)
 
-        startupinfo = None
-        if os.name == 'nt':
-            startupinfo = subprocess.STARTUPINFO()
-            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        try:
+            startupinfo = None
+            if os.name == 'nt':
+                startupinfo = subprocess.STARTUPINFO()
+                startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 
-        subprocess.Popen(['cmd'], 
-                        cwd=path,
-                        startupinfo=startupinfo)
+            print("Starting CMD in:", path)
+            process = subprocess.Popen(
+                'cmd.exe /k cd /d ' + path,
+                cwd=path,
+                startupinfo=startupinfo
+            )
+            print("CMD process started:", process.pid)
+
+        except Exception as e:
+            print("Error starting CMD:", str(e))
+            sublime.error_message("Error starting CMD: " + str(e))
 
     def is_visible(self, dirs=None):
-        return bool(dirs)
+        is_vis = bool(dirs)
+        print("OpenCmdCommand.is_visible:", is_vis, "dirs:", dirs)
+        return is_vis
